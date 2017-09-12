4f1d079bfb6a11581baf2ec1b4fa3545de5e38a6
Nando Ramirez <erbnando@gmail.com>
2017-09-12 00:35:23 -0700

    added improvements for touch screens and page layout text size

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html id="html" <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, minimal-ui, user-scalable=no">		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui, user-scalable=0">		
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 		<title><?php bloginfo( 'name' ); ?></title>
 		<link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i|Poly:400,400i" rel="stylesheet">
 		<?php wp_head(); ?>
