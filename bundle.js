eabdba58973d1ac3cf02684d769191950bb719f2
Nando Ramirez <erbnando@gmail.com>
2017-10-02 21:16:40 -0700

    fix to so that pre tag text wraps

--- bundle.js
+++ bundle.js
@@ -100,7 +100,7 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	__webpack_require__(325);
+	__webpack_require__(324);
 
 	var App = function (_Component) {
 		_inherits(App, _Component);
@@ -34990,8 +34990,7 @@
 
 
 /***/ }),
-/* 324 */,
-/* 325 */
+/* 324 */
 /***/ (function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
